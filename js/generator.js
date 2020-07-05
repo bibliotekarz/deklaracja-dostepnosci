5ed3568d06dc612a65d900742d06727cee25de59
bibliotekarz <arek@bibliotekarz.com>
2020-07-05 15:59:40 +0200

    dodać walidację i posprzątać

--- js/generator.js
+++ js/generator.js
@@ -1,40 +1,172 @@
 function utworz_text() {
 
     let zformularz = new FormData(document.getElementById('formularz'));
-        
+
     if (zformularz.get('hinfo_user').match(/^\s*$/g)) {
         niepelnosprawni = "";
     } else {
-        niepelnosprawni = `<h2>Informacja dla użytkowników niepełnosprawnych</h2> ${zformularz.get('hinfo_user')}`;
+        niepelnosprawni = `<h3>Informacja dla użytkowników niepełnosprawnych</h3> ${zformularz.get('hinfo_user')}`;
     }
 
 
 
-// :TODO: osadzić style bezpośrednio w dokumencie
+    // :TODO: poprawić style ujednolicić i posprzątać
+    // :TODO: walidacja formatu daty
+    // :TODO: walidacja maila
+
     var bazowy = `<html>
         <head>
             <title>Deklaracja dostępności</title>
             <meta name="deklaracja-dostępności" content="${zformularz.get('urldeklaracji')}">    
             <meta charset="UTF-8" />
             <meta name="description" content="Deklaracja dostępności" />
-            <!-- link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" / -->
-            <link rel="stylesheet" href="css/style.css" />
+            <style>
+            body {
+                font-size: .813em;
+                line-height: 1.385em;
+                color: #575757;
+                font-family: Verdana,sans-serif;
+            }
+    
+            #content {
+                width: 90%;
+                background-color: white;
+                padding: 30px;
+            }
+    
+            .site-branding {
+                width: 90%;
+                min-height: 93px;
+                height: 120px;
+                margin: 0 auto;
+                display: table;
+            }
+    
+            #logo {
+                vertical-align: middle;
+                display: table-cell;
+            }
+    
+            a,
+            a:visited {
+                color: #575757;
+            }
+    
+            #logo img {
+                display: block;
+                max-width: none;
+                margin:0 auto;
+            }
+    
+            .site-title a,
+            .site-title a:visited {
+                color: #000;
+                border-color: transparent;
+                border-top-color: transparent;
+                border-right-color: transparent;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+                display: inline-block;
+                text-decoration: none;
+            }
+    
+            .site-title {
+                border: none;
+                display: table-cell;
+                vertical-align: middle;
+                font-family: Mukta;
+                color: #000;
+                line-height: 110%;
+                font-kerning: normal;
+                letter-spacing: 3px;
+                text-transform: uppercase;
+            }
+    
+            #main-content-wrapper {
+                width: 980px;
+                margin: 0 auto;
+                background-color: #fff;
+                -webkit-box-shadow: 0 0 20px 0 rgba(0, 0, 0, .08);
+                /*    -moz-box-shadow: 0 0 20px 0 rgba(0, 0, 0, .08); */
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, .08);
+                position: relative;
+                z-index: 1;
+                display: table-row;
+            }
+            h1 h2 h3 {
+                clear: both;
+                font-weight: 400;
+                color: #154d88;
+                line-height: 1.1em;
+            }
+        
+            h1 {
+                font-size: 2em;
+            }
+    
+            h2 {
+                font-size: 1.5em;
+            }
+            h3 {
+                font-size: 1.17em;
+            }
+            .alert{
+                color: #ff0000;
+            }
+
+    /* formularz */
+    .w3-container, .w3-panel {
+        padding: 0.01em 16px;
+    }
+    .w3-card-4, .w3-hover-shadow:hover {
+        box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);
+    }
+    fieldset {
+        border: 1px solid #c0c0c0;
+        margin: 0 2px;
+        padding: .35em .625em .75em;
+    }
+    legend {
+        color: inherit;
+        display: table;
+        max-width: 100%;
+        padding: 0;
+        white-space: normal;
+    }
+    .w3-text-blue, .w3-hover-text-blue:hover {
+        color: #2196F3 !important;
+    }
+    .w3-border {
+        border: 1px solid #ccc !important;
+    }
+    .w3-input {
+        padding: 8px;
+        display: block;
+        border: none;
+        border-bottom: 1px solid #ccc;
+        width: 100%;
+    }
+    </style>
         </head>
         
         <body>
             <div class="site-branding" style="width: 980px;">
-                
                 <div id="logo">
-                    <a href="${zformularz.get('urlwww')}" rel="home"><img src="${zformularz.get('urllogo')}" alt="Logo ${zformularz.get('podmiot')}"></a>
-                </div><h1 class="site-title"><a href="${zformularz.get('urlwww')}" rel="home"> ${zformularz.get('podmiot')}</a></h1>
+                <a href="${zformularz.get('urlwww')}" rel="home"><img src="${zformularz.get('urllogo')}" alt="Logo ${zformularz.get('podmiot')}"></a>
+                </div>
+                <h1 class="site-title"><a href="${zformularz.get('urlwww')}" rel="home"> ${zformularz.get('podmiot')}</a></h1>      
+
             </div>
             <div id="main-content-wrapper">
+
+
+
                 <div id="content" class="site-content" style="background-color: white; padding: 30px;">
                     <div id="primary" class="content-area index-page" style="padding: 0px;">
                         <main id="main" class="site-main" role="main">
                             <div id="a11y-deklaracja">
-                                <h1>Deklaracja dostępności</h1>
-                                <h2>Wstęp Deklaracji</h2>
+                                <h2>Deklaracja dostępności</h2>
+                                <h3>Wstęp Deklaracji</h3>
                                 <p id="a11y-wstep"><span id="a11y-podmiot">${zformularz.get('podmiot')}</span> zobowiązuje się zapewnić dostępność swojej strony internetowej zgodnie z przepisami ustawy z dnia 4 kwietnia 2019 r. o dostępności cyfrowej stron internetowych i aplikacji mobilnych podmiotów publicznych. Oświadczenie w sprawie dostępności ma zastosowanie do <a href="${zformularz.get('urlwww')}" id="a11y-url" title="Link do strony domowej ${zformularz.get('podmiot')}">strony internetowej ${zformularz.get('podmiot')}.</a></p>
         
                                 <p>Data publikacji strony internetowej: <span id="a11y-data_publikacja">${zformularz.get('data_publikacja')}</span>.</p>
@@ -46,7 +178,7 @@ function utworz_text() {
         
                                 <p>Oświadczenie sporządzono dnia <span id="a11y-data_sporzadzenie">${zformularz.get('data_sporzadzenie')}</span>. ${zformularz.get('hopis_strony')}
                                  ${niepelnosprawni} 
-                                <h2>Informacje zwrotne i dane kontaktowe</h2>
+                                <h3>Informacje zwrotne i dane kontaktowe</h3>
                                 <div id="a11y-procedura">
                                     <div id="a11y-kontakt">
                                         <p>W przypadku problemów z dostępnością strony internetowej prosimy o kontakt. Osobą odpowiedzialną jest <span id="a11y-osoba">${zformularz.get('osoba')}</span> adres poczty elektronicznej <a id="a11y-email" href="${zformularz.get('email')}">${zformularz.get('email')}</a> lub tel. <span id="a11y-telefon">${zformularz.get('telefon')}</span>. Tą samą drogą można składać wnioski o udostępnienie informacji niedostępnej oraz składać skargi na brak zapewnienia dostępności.</p>
@@ -59,7 +191,7 @@ function utworz_text() {
                                     <p>W przypadku, gdy podmiot odmówi realizacji żądania zapewnienia dostępności lub alternatywnego dostępu do informacji, można złożyć skargę na takie działanie.</p>
                                     <p>Po wyczerpaniu wszystkich możliwości skargę można przesłać także do <a href="https://www.rpo.gov.pl/content/jak-zglosic-sie-do-rzecznika-praw-obywatelskich" title="Przekierowanie do strony zewnętrznej">Rzecznika Praw Obywatelskich</a>.</p>
                                 </div>
-                                <h2>Dostępność architektoniczna</h2>
+                                <h3>Dostępność architektoniczna</h3>
                                 <div id="a11y-architektura">${zformularz.get('harchitektura')}</div>
                             </div>
                         </main>
